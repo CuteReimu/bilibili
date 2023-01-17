15d7715963f57b19f39746c77a31581723e774a2
CuteReimu <415551921@qq.com>
2023-01-17 14:25:48 +0800

    获取用户专栏文章列表、获取用户专栏文集列表

--- article.go
+++ article.go
@@ -215,3 +215,170 @@ func (c *Client) FavourArticle(id int) error {
 	_, err = getRespData(resp, "收藏文章")
 	return err
 }
+
+type UserArticleList struct {
+	Articles []struct { // 专栏文章信息列表
+		Id       int      `json:"id"` // 专栏文章id
+		Category struct { // 分类
+			Id       int    `json:"id"`        // 	分类id
+			ParentId int    `json:"parent_id"` // 父级分类id
+			Name     string `json:"name"`      // 分类名称
+		} `json:"category"`
+		Categories []struct { // 分类
+			Id       int    `json:"id"`        // 分类id
+			ParentId int    `json:"parent_id"` // 父级分类id
+			Name     string `json:"name"`      // 分类名称
+		} `json:"categories"`
+		Title      string   `json:"title"`      // 标题
+		Summary    string   `json:"summary"`    // 摘要
+		BannerUrl  string   `json:"banner_url"` // 封面图
+		TemplateId int      `json:"template_id"`
+		State      int      `json:"state"`
+		Author     struct { // UP主信息
+			Mid     int      `json:"mid"`  // 用户uid
+			Name    string   `json:"name"` // 用户名
+			Face    string   `json:"face"` // 头像
+			Pendant struct { // 头像框信息
+				Pid    int    `json:"pid"`    // 头像框id
+				Name   string `json:"name"`   // 头像框名称
+				Image  string `json:"image"`  // 头像框图片url
+				Expire int    `json:"expire"` // 过期时间
+			} `json:"pendant"`
+			OfficialVerify struct { // 账号认证信息
+				Type int    `json:"type"` // 是否认证，-1：无，0：个人认证，1：机构认证
+				Desc string `json:"desc"` // 认证备注
+			} `json:"official_verify"`
+			Nameplate struct { // 成就勋章信息
+				Nid        int    `json:"nid"`         // 勋章id
+				Name       string `json:"name"`        // 勋章名称
+				Image      string `json:"image"`       // 勋章图标
+				ImageSmall string `json:"image_small"` // 勋章图标（小）
+				Level      string `json:"level"`       // 勋章等级
+				Condition  string `json:"condition"`   // 获取条件
+			} `json:"nameplate"`
+			Vip struct { // 大会员信息
+				Type       int      `json:"type"`         // 大会员类型，0：无，1：月大会员，2：年度及以上大会员
+				Status     int      `json:"status"`       // 大会员状态，0：无，1：有
+				DueDate    int      `json:"due_date"`     // 大会员过期时间时间戳，单位：毫秒
+				VipPayType int      `json:"vip_pay_type"` // 支付类型
+				ThemeType  int      `json:"theme_type"`   // 固定值0
+				Label      struct { // 大会员标签
+					Path       string `json:"path"`        // 空串
+					Text       string `json:"text"`        // 会员类型文案，大会员，年度大会员，十年大会员，百年大会员，最强绿鲤鱼
+					LabelTheme string `json:"label_theme"` // 会员标签，vip，annual_vip，ten_annual_vip，hundred_annual_vip，fools_day_hundred_annual_vip
+				} `json:"label"`
+				AvatarSubscript int    `json:"avatar_subscript"` // 是否显示大会员图标，0：不显示，1：显示
+				NicknameColor   string `json:"nickname_color"`   // 大会员昵称颜色
+			} `json:"vip"`
+		} `json:"author"`
+		Reprint     int      `json:"reprint"`
+		ImageUrls   []string `json:"image_urls"`
+		PublishTime int      `json:"publish_time"` // 发布时间戳，单位：秒
+		Ctime       int      `json:"ctime"`        // 提交时间戳，单位：秒
+		Stats       struct { // 专栏文章数据统计
+			View     int `json:"view"`     // 浏览数
+			Favorite int `json:"favorite"` // 收藏数
+			Like     int `json:"like"`     // 点赞数
+			Dislike  int `json:"dislike"`  // 点踩数
+			Reply    int `json:"reply"`    // 回复数
+			Share    int `json:"share"`    // 转发数
+			Coin     int `json:"coin"`     // 投币数
+			Dynamic  int `json:"dynamic"`
+		} `json:"stats"`
+		Words           int      `json:"words"`
+		Dynamic         string   `json:"dynamic"` // 粉丝动态文案
+		OriginImageUrls []string `json:"origin_image_urls"`
+		IsLike          bool     `json:"is_like"`
+		Media           struct {
+			Score    int    `json:"score"`
+			MediaId  int    `json:"media_id"`
+			Title    string `json:"title"`
+			Cover    string `json:"cover"`
+			Area     string `json:"area"`
+			TypeId   int    `json:"type_id"`
+			TypeName string `json:"type_name"`
+			Spoiler  int    `json:"spoiler"`
+		} `json:"media"`
+		ApplyTime string     `json:"apply_time"`
+		CheckTime string     `json:"check_time"`
+		Original  int        `json:"original"`
+		ActId     int        `json:"act_id"`
+		CoverAvid int        `json:"cover_avid"`
+		Type      int        `json:"type"`
+		Tags      []struct { // 标签
+			Tid  int    `json:"tid"`  // 标签id
+			Name string `json:"name"` // 标签名称
+		} `json:"tags,omitempty"`
+	} `json:"articles"`
+	Pn    int `json:"pn"`    // 本次请求分页页数
+	Ps    int `json:"ps"`    // 本次请求分页大小
+	Count int `json:"count"` // 专栏文章总数
+}
+
+// GetUserArticleList 获取用户专栏文章列表。sort可选值"publish_time"，"view"，"fav"，不填默认"publish_time"。
+func GetUserArticleList(mid, pn, ps int, sort string) (*UserArticleList, error) {
+	return std.GetUserArticleList(mid, pn, ps, sort)
+}
+func (c *Client) GetUserArticleList(mid, pn, ps int, sort string) (*UserArticleList, error) {
+	if len(sort) == 0 {
+		sort = "publish_time"
+	}
+	resp, err := c.resty().R().SetHeader("Content-Type", "application/x-www-form-urlencoded").SetQueryParams(map[string]string{
+		"mid":  strconv.Itoa(mid),
+		"pn":   strconv.Itoa(pn),
+		"ps":   strconv.Itoa(ps),
+		"sort": sort,
+	}).Get("https://api.bilibili.com/x/space/article")
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	data, err := getRespData(resp, "获取用户专栏文章列表")
+	if err != nil {
+		return nil, err
+	}
+	var ret *UserArticleList
+	err = json.Unmarshal(data, &ret)
+	return ret, errors.WithStack(err)
+}
+
+type UserArticlesList struct {
+	Lists []struct { // 文集信息列表
+		Id            int    `json:"id"`           // 文集id
+		Mid           int    `json:"mid"`          // 作者uid
+		Name          string `json:"name"`         // 文集名称
+		ImageUrl      string `json:"image_url"`    // 封面
+		UpdateTime    int    `json:"update_time"`  // 最后更新时间戳，单位：秒
+		Ctime         int    `json:"ctime"`        // 创建时间戳，单位：秒
+		PublishTime   int    `json:"publish_time"` // 发布时间戳，单位：秒
+		Summary       string `json:"summary"`
+		Words         int    `json:"words"`          // 总字数
+		Read          int    `json:"read"`           // 阅读量
+		ArticlesCount int    `json:"articles_count"` // 包含文章数
+		State         int    `json:"state"`          // 固定值1
+		Reason        string `json:"reason"`
+		ApplyTime     string `json:"apply_time"`
+		CheckTime     string `json:"check_time"`
+	} `json:"lists"`
+	Total int `json:"total"` // 文集总数
+}
+
+// GetUserArticlesList 获取用户专栏文集列表。sort可选值，0：最近更新，1：最多阅读。
+func GetUserArticlesList(mid, sort int) (*UserArticlesList, error) {
+	return std.GetUserArticlesList(mid, sort)
+}
+func (c *Client) GetUserArticlesList(mid, sort int) (*UserArticlesList, error) {
+	resp, err := c.resty().R().SetHeader("Content-Type", "application/x-www-form-urlencoded").SetQueryParams(map[string]string{
+		"mid":  strconv.Itoa(mid),
+		"sort": strconv.Itoa(sort),
+	}).Get("https://api.bilibili.com/x/article/up/lists")
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	data, err := getRespData(resp, "获取用户专栏文集列表")
+	if err != nil {
+		return nil, err
+	}
+	var ret *UserArticlesList
+	err = json.Unmarshal(data, &ret)
+	return ret, errors.WithStack(err)
+}
