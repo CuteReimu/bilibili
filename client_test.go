f45821d81674f35241c94a7673b96855a61aafbd
CuteReimu <415551921@qq.com>
2024-05-10 11:44:22 +0800

    Update client_test.go

--- client_test.go
+++ client_test.go
@@ -2,32 +2,58 @@ package bilibili
 
 import (
 	"net/http"
-	"reflect"
+	"strings"
 	"testing"
 )
 
+func deepEquals(a, b []*http.Cookie) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i].Name != b[i].Name {
+			return false
+		}
+		if a[i].Value != b[i].Value {
+			return false
+		}
+		if a[i].Domain != b[i].Domain {
+			return false
+		}
+		if a[i].Path != b[i].Path {
+			return false
+		}
+	}
+	return true
+}
+
 func TestCookie(t *testing.T) {
-	result := []*http.Cookie{
-		{Name: "a", Value: "1"},
-		{Name: "b", Value: "2"},
+	{
+		result := []*http.Cookie{ // 不测试Expires，因为time.Time转化成string再转化回来会丢失单调时钟
+			{Name: "a", Value: "1", Domain: "bilibili.com", Path: "/"},
+			{Name: "b", Value: "2", Domain: "bilibili.com", Path: "/"},
+		}
+		s := make([]string, 0, len(result))
+		for _, cookie := range result {
+			s = append(s, cookie.String())
+		}
+		c := New()
+		c.SetCookiesString(strings.Join(s, "\n"))
+		if !deepEquals(result, c.GetCookies()) {
+			t.Fail()
+		}
 	}
 	{
+		result := []*http.Cookie{
+			{Name: "a", Value: "1"},
+			{Name: "b", Value: "2"},
+		}
 		c := New()
-		c.SetCookiesString("a=1; b=2")
-		if c.GetCookiesString() != "a=1; b=2" {
+		c.SetRawCookies("a=1; b=2")
+		if c.GetCookiesString() != "a=1\nb=2" {
 			t.Fail()
 		}
-		if !reflect.DeepEqual(c.GetCookies(), result) {
-			t.Fail()
-		}
-	}
-	{
-		c := New()
-		c.SetCookiesString("a=1\nb=2")
-		if c.GetCookiesString() != "a=1; b=2" {
-			t.Fail()
-		}
-		if !reflect.DeepEqual(c.GetCookies(), result) {
+		if !deepEquals(result, c.GetCookies()) {
 			t.Fail()
 		}
 	}
