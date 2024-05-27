f28a75b67e7b56e8f94328b104a71f0b437f0d80
云奕泽 <59525863+yunyizea@users.noreply.github.com>
2024-05-27 12:14:06 +0800

    Update message.go
    将talk_id更改为必填项

--- message.go
+++ message.go
@@ -89,7 +89,7 @@ func (c *Client) SendPrivateMessage(param SendPrivateMessageParam) (*SendPrivate
 }
 
 type GetPrivateMessageRecordsParam struct {
-	TalkerId       int    `json:"talker_id" request:"query,omitempty"`                  // 聊天对象的uid
+	TalkerId       int    `json:"talker_id"`                                            // 聊天对象的uid
 	SenderDeviceId int    `json:"sender_device_id,omitempty" request:"query,omitempty"` // 发送者设备。1
 	SessionType    int    `json:"session_type"`                                         // 聊天对象的类型。1为用户，2为粉丝团
 	Size           int    `json:"size,omitempty" request:"query,omitempty"`             // 列出消息条数。默认是20，最大为200
