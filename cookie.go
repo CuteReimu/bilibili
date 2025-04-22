47fd9645b50edc0f3f6662e764c847b416df2726
RongPengju <gophercoding@gmail.com>
2025-04-22 16:10:13 +0800

    feat: 获取cookie刷新信息

---
+++ cookie.go
@@ -0,0 +1,20 @@
+package bilibili
+
+import (
+	"github.com/go-resty/resty/v2"
+)
+
+type GetWebCookieRefreshInfoResult struct {
+	Refresh   bool  `json:"refresh"`   // 是否应该刷新 Cookie。true-需要刷新，false-不需要刷新
+	Timestamp int64 `json:"timestamp"` // 用于获取 refresh_csrf 的毫秒时间戳
+}
+
+// GetWebCookieRefreshInfo 获取web端cookie刷新信息
+func (c *Client) GetWebCookieRefreshInfo() (*GetWebCookieRefreshInfoResult, error) {
+	const (
+		method = resty.MethodGet
+		url    = "https://passport.bilibili.com/x/passport-login/web/cookie/info"
+	)
+
+	return execute[*GetWebCookieRefreshInfoResult](c, method, url, nil)
+}
